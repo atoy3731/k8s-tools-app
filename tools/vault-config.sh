1d125200b5c181976f1779d6ce1bf72bcb2b6b6f
Adam Toy <adam.toy@rancherfederal.com>
2020-10-12 20:46:57 -0400

    add

---
+++ tools/vault-config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+kubectl get secret -n vault vault-tls -o jsonpath="{.data.ca\.crt}" | base64 --decode > ~/.vault-ca.crt
+VAULT_TOKEN=$(kubectl get secrets -n vault vault-unseal-keys -o jsonpath={.data.vault-root} | base64 --decode)
+
+echo "Run the following:"
+echo "export VAULT_TOKEN=$VAULT_TOKEN"
+echo "export VAULT_CACERT=$HOME/.vault-ca.crt"
+echo "kubectl port-forward -n vault service/vault 8200 &"
